0522b3e0177ebc8a39cc30e073d77bdffb03c95a
Rishabh Dwivedi <rishabhdwivedi17@gmail.com>
2020-11-07 18:28:02 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# nvim-gruvbox
+
+Heavily inspired from [tjdevries/gruvbuddy.nvim](https://github.com/tjdevries/gruvbuddy.nvim)
+
+(My desired changes to gruvbuddy.)
+
+Gruvbox escape sequenced colorscheme.
+
+![screen](./media/nvim-gruvbox.png)
+
+This plugin is also an example of how easy it is to customize and make new colorschemes based on `colorbuddy`.
+
+Check out `lua/nvim-gruvbox.lua`
+
+## Installation
+
+```vim
+Plug 'tjdevries/colorbuddy.vim'
+Plug 'RishabhRD/nvim-gruvbox'
+
+" And then somewhere in your vimrc, to set the colorscheme
+lua require('colorbuddy').colorscheme('nvim-gruvbox')
+```
---
+++ lua/nvim-gruvbox.lua
@@ -0,0 +1,25 @@
+local Color, colors, Group, _, styles = require('colorbuddy').setup()
+local no = styles.NONE
+
+Color.new('white',     '#f2e5bc')
+Color.new('red',       '#cc6666')
+Color.new('pink',      '#fef601')
+Color.new('green',     '#99cc99')
+Color.new('yellow',    '#f8fe7a')
+Color.new('blue',      '#81a2be')
+Color.new('aqua',      '#8ec07c')
+Color.new('cyan',      '#8abeb7')
+Color.new('purple',    '#8e6fbd')
+Color.new('violet',    '#b294bb')
+Color.new('orange',    '#de935f')
+Color.new('brown',     '#a3685a')
+
+Color.new('seagreen',  '#698b69')
+Color.new('turquoise', '#698b69')
+Color.new('turquoise', '#4d4a4a')
+Color.new('grey', '#313332')
+Color.new('syntax_bg', '#282c34')
+Color.new('syntax_fg', '#d9d7d7')
+
+Group.new('ColorColumn', colors.none, colors.grey,  no)
+Group.new('Normal', colors.syntax_fg, colors.syntax_bg, no)
